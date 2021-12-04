04307f58a01a6333b140823784a42ce901d0fdda
Simonbelete <simonbelete@gmail.com>
2021-12-04 23:02:13 +0300

    aligning acessorize using flex

--- alpaca-image-generator/src/components/Accessorize/index.tsx
+++ alpaca-image-generator/src/components/Accessorize/index.tsx
@@ -4,15 +4,21 @@ import { Button } from "components";
 
 const Accessorize: React.FC = (): ReactElement => {
   return (
-    <div className="grid grid-cols-3 gap-1 auto-cols-max">
-      <Button active={true}>Hair</Button>
-      <Button>Ears</Button>
-      <Button>Eyes</Button>
-      <Button>Mouth</Button>
-      <Button>Neck</Button>
-      <Button>Leg</Button>
-      <Button>Accessories</Button>
-      <Button>Background</Button>
+    <div className="space-y-2">
+      <div className="space-x-1">
+        <Button>Hair</Button>
+        <Button>Ears</Button>
+        <Button>Eyes</Button>
+      </div>
+      <div className="space-x-1">
+        <Button>Mouth</Button>
+        <Button>Neck</Button>
+        <Button>Leg</Button>
+      </div>
+      <div className="space-x-1">
+        <Button>Accessories</Button>
+        <Button>Background</Button>
+      </div>
     </div>
   );
 };
--- alpaca-image-generator/src/components/Button/index.tsx
+++ alpaca-image-generator/src/components/Button/index.tsx
@@ -1,17 +1,20 @@
 import React, { ReactElement } from "react";
 
-const Button: React.FC<{ active?: boolean; children: string | ReactElement }> =
-  ({ active = false, children }): ReactElement => {
-    return (
-      <button
-        type="button"
-        className={` font-bold border-light_purple rounded-full py-2 px-5 hover:border-purple hover:text-purple ${
-          active ? "bg-purple text-white" : "text-light_purple border-2"
-        }`}
-      >
-        <>{children}</>
-      </button>
-    );
-  };
+const Button: React.FC<{
+  active?: boolean;
+  children: string | ReactElement;
+  className?: string;
+}> = ({ active = false, children, className }): ReactElement => {
+  return (
+    <button
+      type="button"
+      className={`${className} font-bold border-light_purple rounded-full py-2 px-7 hover:border-purple hover:text-purple ${
+        active ? "bg-purple text-white" : "text-light_purple border-2"
+      }`}
+    >
+      <>{children}</>
+    </button>
+  );
+};
 
 export default Button;
