2c62ab0900363554a0f1815bc287d316fe301ab4
Simonbelete <simonbelete@gmail.com>
2021-12-15 13:52:50 +0300

    alpaca - more actions

--- alpaca-image-generator/src/reducers/index.ts
+++ alpaca-image-generator/src/reducers/index.ts
@@ -2,6 +2,13 @@ const INITIAL_STATE = {
   url: "/backgrounds/darkblue70.png",
   selected: "HAIR",
   style: "HAIR_DEFAULT",
+  hair: "/hair/default.png",
+  ear: "ears/default.png",
+  eye: "eyes/default.png",
+  mouth: "mouth/default.png",
+  neck: "neck/default.png",
+  leg: "leg/default.png",
+  accessorie: "accessories/headphone.png",
 };
 
 const CommonReducer = (state = INITIAL_STATE, action: any) => {
@@ -10,18 +17,130 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
       url: action.payload.url,
       selected: state.selected,
       style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
     };
   else if (action.payload && action.payload.selected)
     return {
       url: state.url,
       selected: action.payload.selected,
       style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
     };
   else if (action.payload && action.payload.style)
     return {
       url: state.url,
       selected: state.selected,
+      style: action.payload.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.hair)
+    return {
+      url: state.url,
+      selected: state.selected,
       style: state.style,
+      hair: action.payload.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.ear)
+    return {
+      url: state.url,
+      selected: state.selected,
+      style: state.style,
+      hair: state.hair,
+      ear: action.payload.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.eye)
+    return {
+      url: state.url,
+      selected: state.selected,
+      style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: action.payload.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.mouth)
+    return {
+      url: state.url,
+      selected: state.selected,
+      style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: action.payload.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.neck)
+    return {
+      url: state.url,
+      selected: state.selected,
+      style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: action.payload.neck,
+      leg: state.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.leg)
+    return {
+      url: state.url,
+      selected: state.selected,
+      style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: action.payload.leg,
+      accessorie: state.accessorie,
+    };
+  else if (action.payload && action.payload.accessorie)
+    return {
+      url: state.url,
+      selected: state.selected,
+      style: state.style,
+      hair: state.hair,
+      ear: state.ear,
+      eye: state.eye,
+      mouth: state.mouth,
+      neck: state.neck,
+      leg: state.leg,
+      accessorie: action.payload.accessorie,
     };
   else return state;
 };
