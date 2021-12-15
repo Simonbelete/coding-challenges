2a4c6ab87a43995fa55bda3c666470ddbb9c6da5
Simonbelete <simonbelete@gmail.com>
2021-12-15 17:18:38 +0300

    alpaca - fixing common reducer

--- alpaca-image-generator/src/reducers/index.ts
+++ alpaca-image-generator/src/reducers/index.ts
@@ -38,24 +38,11 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
       leg: state.leg,
       accessorie: state.accessorie,
     };
-  else if (action.payload && action.payload.style)
-    return {
-      url: state.url,
-      selected: state.selected,
-      style: action.payload.style,
-      hair: state.hair,
-      ear: state.ear,
-      eye: state.eye,
-      mouth: state.mouth,
-      neck: state.neck,
-      leg: state.leg,
-      accessorie: state.accessorie,
-    };
   else if (action.payload && action.payload.hair)
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: action.payload.hair,
       ear: state.ear,
       eye: state.eye,
@@ -68,7 +55,7 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: state.hair,
       ear: action.payload.ear,
       eye: state.eye,
@@ -81,7 +68,7 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: state.hair,
       ear: state.ear,
       eye: action.payload.eye,
@@ -94,7 +81,7 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: state.hair,
       ear: state.ear,
       eye: state.eye,
@@ -107,7 +94,7 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: state.hair,
       ear: state.ear,
       eye: state.eye,
@@ -120,7 +107,7 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: state.hair,
       ear: state.ear,
       eye: state.eye,
@@ -133,7 +120,7 @@ const CommonReducer = (state = INITIAL_STATE, action: any) => {
     return {
       url: state.url,
       selected: state.selected,
-      style: state.style,
+      style: action.payload.style,
       hair: state.hair,
       ear: state.ear,
       eye: state.eye,
